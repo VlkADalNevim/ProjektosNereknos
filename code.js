5cbf18f1d9c991d457b6700ad1ab6941214bd786
VlkADalNevim <104823072+VlkADalNevim@users.noreply.github.com>
2022-05-03 09:45:52 +0200

    Add files via upload

---
+++ code.js
@@ -0,0 +1,8 @@
+function openRightMenu() {
+    document.getElementById("rightMenu").style.display = "block";
+  }
+  
+  function closeRightMenu() {
+    document.getElementById("rightMenu").style.display = "none";
+  }
+  
